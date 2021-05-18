ccd93350a54248c7bc0c438f3c9e82a28ddda228
AnneAugust606 <82916149+AnneAugust606@users.noreply.github.com>
2021-05-18 14:04:34 -0600

    troubleshooting

--- src/Icon.js
+++ src/Icon.js
@@ -4,39 +4,39 @@ import "./Icon.css";
 export default function Icon(props) {
   let icon = "";
   if (props.icon === "03d" || props.icon === "03n") {
-    icon = "Icons/03dn.svg"; //scattered clouds day/night
+    icon = "./Icons/03dn.svg"; //scattered clouds day/night
   } else if (props.icon === "04d") {
-    icon = "Icons/03dn.svg"; //broken clouds day
+    icon = "./Icons/03dn.svg"; //broken clouds day
   } else if (props.icon === "04n") {
-    icon = "Icons/03dn.svg"; //broken clouds night
+    icon = "./Icons/03dn.svg"; //broken clouds night
   } else if (props.icon === "01d") {
-    icon = "Icons/03dn.svg"; //clear day
+    icon = "./Icons/03dn.svg"; //clear day
   } else if (props.icon === "01n") {
-    icon = "Icons/03dn.svg"; //clear night
+    icon = "./Icons/03dn.svg"; //clear night
   } else if (props.icon === "02d") {
-    icon = "Icons/03dn.svg"; //partly cloudy day
+    icon = "./Icons/03dn.svg"; //partly cloudy day
   } else if (props.icon === "02n") {
-    icon = "Icons/03dn.svg"; //partly cloudy night
+    icon = "./Icons/03dn.svg"; //partly cloudy night
   } else if (props.icon === "09d") {
-    icon = "Icons/03dn.svg"; //showers day
+    icon = "./Icons/03dn.svg"; //showers day
   } else if (props.icon === "09n") {
-    icon = "Icons/03dn.svg"; //showers night
+    icon = "./Icons/03dn.svg"; //showers night
   } else if (props.icon === "10d") {
-    icon = "Icons/03dn.svg"; //mod-heavy rain day
+    icon = "./Icons/03dn.svg"; //mod-heavy rain day
   } else if (props.icon === "10n") {
-    icon = "Icons/03dn.svg"; //rain night
+    icon = "./Icons/03dn.svg"; //rain night
   } else if (props.icon === "11d") {
-    icon = "Icons/03dn.svg"; //thunderstorm day
+    icon = "./Icons/03dn.svg"; //thunderstorm day
   } else if (props.icon === "11n") {
-    icon = "Icons/03dn.svg"; //thunderstorm night
+    icon = "./Icons/03dn.svg"; //thunderstorm night
   } else if (props.icon === "13d") {
-    icon = "Icons/03dn.svg"; //snow day
+    icon = "./Icons/03dn.svg"; //snow day
   } else if (props.icon === "13n") {
-    icon = "Icons/03dn.svg"; //snow night
+    icon = "./Icons/03dn.svg"; //snow night
   } else if (props.icon === "50d") {
-    icon = "Icons/03dn.svg"; //mist day
+    icon = "./Icons/03dn.svg"; //mist day
   } else if (props.icon === "50n") {
-    icon = "Icons/03dn.svg"; //mist night
+    icon = "./Icons/03dn.svg"; //mist night
   }
 
   return <img src={icon} alt="weather-icon" />;
